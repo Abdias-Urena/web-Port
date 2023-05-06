8f57636a3467fa7a0ca15cd397b33c92a627a420
Abdías Ureña <abdias.urena.soto@est.una.ac.cr>
2023-05-05 22:58:35 -0600

    Update CallToAction.tsx

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CallToAction = () => (
   <section className="call-to-action bg-light py-5">
-    <div className="container text-center">
+    <div className="container text-center" id="contracts">
       <h2 className="mb-4">Ready to start?</h2>
       <p className="mb-4">Contact me right now to discuss your project.</p>
       <button className="btn btn-primary btn-lg">Hire me!</button>
